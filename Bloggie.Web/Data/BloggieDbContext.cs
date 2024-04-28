00e574e165ddb5ef697b04bbd7552678ad80cf5d
connect2avni <smrutictc100@gmail.com>
2024-04-28 17:21:29 +0530

    Add project files.

---
+++ Bloggie.Web/Data/BloggieDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Bloggie.Web.Models.Domain;
+
+namespace Bloggie.Web.Data
+{
+    public class BloggieDbContext : DbContext
+    {
+        public BloggieDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet <Tag> Tags { get; set; }
+    }
+}
